7e525803f3e090f95809fd34b4e2bb52f3fbcd15
T807970 <T807970@tokiomarine.com.br>
2025-07-20 20:19:24 -0300

    full-commit

---
+++ infoDB/src/controller/GenerateTableMetadataController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.util.List;
+import model.entity.TableMetadata;
+import model.service.TableMetadataService;
+import model.service.error.ErrorMessage;
+
+/**
+ *
+ * @author Fábio Fernandes
+ * @version 1.0
+ */
+public class GenerateTableMetadataController {
+
+    /**
+     * Recupera uma lista de TableMetadata.
+     * 
+     * @return tmList lista de tabelas.
+     */
+    public List<TableMetadata> generate() {
+        try {
+            TableMetadataService tmService = new TableMetadataService();
+            List<TableMetadata> tmList = tmService.getTableMetadata();
+            return tmList;
+        } catch (RuntimeException e) {
+            throw new RuntimeException(ErrorMessage.ERROR_TABLE_METADATA.concat(ErrorMessage.ERROR_EXCEPTION).concat(e.getMessage()));
+        }
+    }
+
+}
